0a4c95fc061d3924ca1b9201d22c8b60ceb36e17
Tony Mele <TonyMele.Rutgers@gmail.com>
2021-08-21 08:26:08 -0400

    more

--- src/components/game/BoardGame.js
+++ src/components/game/BoardGame.js
@@ -4,12 +4,106 @@ import PropTypes from 'prop-types';
 import useImage from 'use-image';
 import $ from "jquery";
 
+// Design
+import { makeStyles,createTheme,ThemeProvider  } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+import SupervisedUserCircleIcon from '@material-ui/icons/SupervisedUserCircle';
+import PersonIcon from '@material-ui/icons/People';
+import LaptopChromebookIcon from '@material-ui/icons/LaptopChromebook';
+import Card from '@material-ui/core/Card';
+import Container from '@material-ui/core/Container';
+import CardActions from '@material-ui/core/CardActions';
+import CardHeader from '@material-ui/core/CardHeader';
+import CardContent from '@material-ui/core/CardContent';
+import Typography from '@material-ui/core/Typography';
+import Grid from '@material-ui/core/Grid';
+import ButtonGroup from '@material-ui/core/ButtonGroup';
+import Modal from '@material-ui/core/Modal';
+import Backdrop from '@material-ui/core/Backdrop';
+import { useSpring, animated } from 'react-spring/web.cjs'; // web.cjs is required for IE 11 support
+
 // Chess Engine from Chess.js
 const chessMoves = require('chess');
 const moveEngine = chessMoves.create({ PGN : true });
 const blackCapturedWhite = Array()
 const whiteCapturedBlack = Array()
 const captured = Array()
+
+const theme = createTheme({
+  palette: {
+    type: "dark",
+  },
+});
+
+const useStylesModal = makeStyles((theme) => ({
+  modal: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  paper: {
+    backgroundColor: theme.palette.background.paper,
+    border: '2px solid #000',
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(2, 4, 3),
+  },
+}));
+const useStylesButtonGroup = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    '& > *': {
+      margin: theme.spacing(1),
+    },
+  },
+}));
+const useStylesContainer = makeStyles((theme) => ({
+  container: {
+    display: "flex",
+    justifyContent: "center",
+  },
+}));
+const useStylesButton = makeStyles((theme) => ({
+    root: {
+        '& > *': {
+            margin: theme.spacing(1),
+        },
+    },
+    button: {
+        margin: theme.spacing(1),
+    },
+}));
+const useStylesCard = makeStyles((theme) => ({
+  root: {
+    minWidth: 275,
+      paddingBottom: "0px !important"
+  },
+  bullet: {
+    display: '',
+    margin: '0 2px',
+    transform: 'scale(0.8)',
+      paddingBottom: "0px !important"
+  },
+  title: {
+    fontSize: 14,
+      paddingBottom: "0px !important"
+  },
+  pos: {
+    marginBottom: 1,
+      paddingBottom: "0px !important"
+  },
+}));
+const useStylesGrid = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+  },
+}));
 
 // Component which draws game board, renders the 
 // chess pieces and simulates a game using the move engine
@@ -18,6 +112,23 @@ const ChessSet = (props) => {
     // Set Board Map to a State Variable
     const [boardMap, updateBoardMap] = useState(props.game.positions)
 
+    const [open, setOpen] = React.useState(false);
+
+    const handleOpenModal = () => {
+        setOpen(true);
+    };
+
+    const handleCloseModal = () => {
+        setOpen(false);
+    };
+
+    const classesButton = useStylesButton();
+    const classesCard = useStylesCard();
+    const classesGrid = useStylesGrid();
+    const classesButtonGroup = useStylesButtonGroup();
+    const classesContainer = useStylesContainer();
+    const classesModal = useStylesModal();
+
     // Int for selecting a random move
     function getRandomInt(min, max) {
         min = Math.ceil(min);
@@ -25,9 +136,88 @@ const ChessSet = (props) => {
         return Math.floor(Math.random() * (max - min + 1)) + min;
     }
 
-
     // Simulate a Random Walk Chess Game
     function simulateGame() {
+
+        let mv = [];
+
+        setTimeout(function (d,i) {
+            let status = moveEngine.getStatus();
+            let validMoves = status.notatedMoves;
+
+            Object.keys(moveEngine.notatedMoves).forEach((notation) => { mv.push(notation) });
+            console.log("Valid MOVES", validMoves)
+            console.log("Valid MOVES len", validMoves.length)
+
+            const num = getRandomInt(0,mv.length-1)
+            console.log("Move Index is", num)
+
+            const move = validMoves[mv[num]]
+            console.log("The move", move)
+
+            let source = move.src.file+move.src.rank
+            let target = move.dest.file+move.dest.rank
+
+            console.log("Intended Source", source)
+            console.log("Intended Target", target)
+
+            moveEngine.move(mv[num])
+            Move(source, target)
+
+            simulateGame();
+
+            }, 3000);
+
+    }
+
+    // Simulate a Random Walk Chess Game
+    function singlePlayerGame() {
+        handleOpenModal()
+        const opponentPlayers = {
+         10: "Alexander Alekhine",
+          9: "Mikhail Tal",
+          8: "Emanuel Lasker",
+          7: "Vladimir Kramnik",
+          6: "Mikhail Botvinnik",
+          5: "Anatoly Karpov",
+          4: "Jose Raul Capablanca",
+          3: "Bobby Fischer",
+          2: "Magnus Carlsen",
+          1: "Garry Kasparov",
+        }
+        let mv = [];
+
+        setTimeout(function (d,i) {
+            let status = moveEngine.getStatus();
+            let validMoves = status.notatedMoves;
+
+            Object.keys(moveEngine.notatedMoves).forEach((notation) => { mv.push(notation) });
+            console.log("Valid MOVES", validMoves)
+            console.log("Valid MOVES len", validMoves.length)
+
+            const num = getRandomInt(0,mv.length-1)
+            console.log("Move Index is", num)
+
+            const move = validMoves[mv[num]]
+            console.log("The move", move)
+
+            let source = move.src.file+move.src.rank
+            let target = move.dest.file+move.dest.rank
+
+            console.log("Intended Source", source)
+            console.log("Intended Target", target)
+
+            moveEngine.move(mv[num])
+            Move(source, target)
+
+            simulateGame();
+
+            }, 3000);
+
+    }
+
+    // Simulate a Random Walk Chess Game
+    function multiPlayerGame() {
 
         let mv = [];
 
@@ -167,6 +357,7 @@ const ChessSet = (props) => {
             )
         }
     }
+
     // Move Pieces Programmatically
     function Move(src, tgt, cls){
         let boardMapIndexSource = boardMap.findIndex(x => x.cell === src);
@@ -276,11 +467,42 @@ const ChessSet = (props) => {
 
     // Returns the board
      return  (
-        <React.Fragment>
-            <button key={"BUTTON"} onClick={event => simulateGame()}>Simulate Game</button>
-            <Board key={"BOARD"} boardMap={boardMap} square={props.game.boardHeight} piece={props.game.chessmenSize} tileA={props.game.colorA} tileB={props.game.colorB}/>
-            {/*<Captured key={"CAPTURES"} captures={captured} width={props.game.boardWidth}/>*/}
-        </React.Fragment>
+         <ThemeProvider theme={theme}>
+             <br/>
+            <Grid container direction="row" justifyContent="center" className={classesGrid.root} alignItems="center" spacing={3}>
+
+                <Card variant="outlined" border={3} className={classesCard.root} borderRadius="50%">
+                    <CardHeader  title="Championship Chess" subheader="One Pawn at a Time" style={{marginBottom: 0}} />
+
+                    <CardContent>
+                        <Board key={"BOARD"} boardMap={boardMap} square={props.game.boardHeight} piece={props.game.chessmenSize} tileA={props.game.colorA} tileB={props.game.colorB}/>
+                    </CardContent>
+
+                    <CardActions>
+                        <Container className={classesContainer}>
+                         <div className={classesButtonGroup.root}>
+                          <ButtonGroup size="large" color="primary" aria-label="large outlined primary button group">
+                            <Button variant="contained" color="secondary" key={"B1"} className={classesButton.button} startIcon={<LaptopChromebookIcon />} onClick={event => simulateGame()}>Simulate Game</Button>
+                            <>
+                                <Button variant="contained" color="primary" key={"B2"} className={classesButton.button} startIcon={<PersonIcon />} onClick={event => singlePlayerGame()}>Single Player</Button>
+                                <Modal aria-labelledby="spring-modal-title" aria-describedby="spring-modal-description" className={classesModal.modal} open={open} onClose={handleCloseModal} closeAfterTransition BackdropComponent={Backdrop} BackdropProps={{ timeout: 500, }} >
+                                    <Fade in={open}>
+                                        <div className={classesModal.paper}>
+                                        <h2 id="spring-modal-title">Spring modal</h2>
+                                        <p id="spring-modal-description">react-spring animates me.</p>
+                                        </div>
+                                    </Fade>
+                                </Modal>
+                            </>
+                            <Button variant="contained" color="primary" key={"B3"} className={classesButton.button} startIcon={<SupervisedUserCircleIcon />} onClick={event => multiPlayerGame()}>Multi-Player</Button>
+                          </ButtonGroup>
+                        </div>
+                        </Container>
+                    </CardActions>
+
+               </Card>
+             </Grid>
+         </ThemeProvider>
     )
 
 
